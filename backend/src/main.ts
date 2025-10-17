df459f72201ab63ea2b587cc1d25b25efe5de6de
Bizimana Ibrahim <bizimanaibrahim0@gmail.com>
2025-10-17 17:31:18 +0200

    fix: "enabled CORS" for all

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  });
   app.useGlobalPipes(new ValidationPipe({ forbidNonWhitelisted: true }));
   await app.listen(process.env.PORT ?? 3000);
 
